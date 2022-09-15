e812d8cc1f2595a404130e753f961e4670380a3c
natalimj <natalimj88@gmail.com>
2022-09-15 21:22:20 +0200

    web.config file

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:8080/",
+  //baseURL: "http://localhost:8080/",
+  baseURL: "https://tricproject.azurewebsites.net/",
   headers: {
     "Content-type": "application/json"
   }
